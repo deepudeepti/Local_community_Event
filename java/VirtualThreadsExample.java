12c81634ccd40d9e98b25d7fe36009481a9e27b1
deepudeepti <229x1a0596@gprec.ac.in>
2025-05-27 12:48:52 +0530

    Create  VirtualThreadsExample.java

---
+++ java/VirtualThreadsExample.java
@@ -0,0 +1,8 @@
+// Virtual Threads (Java 21)
+public class VirtualThreadsExample {
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 100_000; i++) {
+            Thread.startVirtualThread(() -> System.out.println("Hello from virtual thread!"));
+        }
+    }
+}
