124930f260e23085c81793ccdba1a98beefba817
deepudeepti <229x1a0596@gprec.ac.in>
2025-05-27 12:40:11 +0530

    Create LambdaSortExample.java

---
+++ java/LambdaSortExample.java
@@ -0,0 +1,10 @@
+// Lambda Expressions
+import java.util.*;
+
+public class LambdaSortExample {
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList("Banana", "Apple", "Orange");
+        Collections.sort(list, (a, b) -> a.compareTo(b));
+        System.out.println("Sorted list: " + list);
+    }
+}
